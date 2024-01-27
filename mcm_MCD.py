71bc853eb9768a0d2fd11f6cade3b33b009c8c8f
jdgamarra <jdgamarra@uncp.edu.pe>
2024-01-27 13:38:03 -0500

    Actualizando de los indicadores en el ingreso

--- mcm_MCD.py
+++ mcm_MCD.py
@@ -2,8 +2,8 @@ from NumerosNaturales import NumerosNaturales
 
 if __name__ == '__main__':
     numeroNatural = NumerosNaturales()
-    num1 = int(input("Ingresa un número entero para num1: "))
-    num2 = int(input("Ingresa un número entero para num2: "))
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
 
     print(f"El MCD de {num1} y {num2} es: {numeroNatural.MCD(num1, num2)}")
     print(f"El mcm de {num1} y {num2} es: {numeroNatural.mcm(num1, num2)}")
